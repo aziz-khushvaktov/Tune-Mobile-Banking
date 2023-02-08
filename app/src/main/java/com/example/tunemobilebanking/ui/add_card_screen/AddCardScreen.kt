3e30db806ea89a45c0424e75c2158dfbd0c0cd11
aziz0675 <xua57570@gmail.com>
2023-02-08 13:51:21 +0500

    UI part is completely finished

--- app/src/main/java/com/example/tunemobilebanking/ui/add_card_screen/AddCardScreen.kt
+++ app/src/main/java/com/example/tunemobilebanking/ui/add_card_screen/AddCardScreen.kt
@@ -1,10 +1,14 @@
 package com.example.tunemobilebanking.ui.add_card_screen
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.navigation.fragment.findNavController
 import com.example.tunemobilebanking.R
 import com.example.tunemobilebanking.databinding.FragmentAddCardScreenBinding
 
@@ -19,11 +23,34 @@ class AddCardScreen : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         initViews()
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return binding.root
     }
 
     private fun initViews() {
 
+        binding.ivBack.setOnClickListener {
+            findNavController().navigate(R.id.homeScreen)
+        }
+
+
+        val textWatcher: TextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val cardNumber = binding.etCardNumber.text.toString()
+                val expireDate = binding.etExpireDate.text.toString()
+                binding.bContinue.isEnabled = cardNumber.length == 19 && expireDate.length == 5
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+        binding.etCardNumber.addTextChangedListener(textWatcher)
+        binding.etExpireDate.addTextChangedListener(textWatcher)
     }
 
 }
--- app/src/main/java/com/example/tunemobilebanking/ui/home_screen/HomeScreen.kt
+++ app/src/main/java/com/example/tunemobilebanking/ui/home_screen/HomeScreen.kt
@@ -11,7 +11,6 @@ import com.example.tunemobilebanking.R
 import com.example.tunemobilebanking.databinding.FragmentHomeScreenBinding
 import com.example.tunemobilebanking.ui.home_screen.HomeScreenAdapter
 
-
 class HomeScreen : Fragment() {
 
     private val binding by lazy { FragmentHomeScreenBinding.inflate(layoutInflater) }
@@ -31,18 +30,28 @@ class HomeScreen : Fragment() {
         binding.ivAdd.setOnClickListener { findNavController().navigate(R.id.addCardScreen) }
 
         val list = ArrayList<Card>()
-        for(i in 0..10) {
+        for (i in 0..10) {
             list.add(Card())
         }
 
         adapter.submitList(list)
         binding.rvCards.adapter = adapter
 
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(resources.getString(R.string.str_hammasi)))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(resources.getString(R.string.str_sevimli)))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(resources.getString(R.string.str_uzcard)))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(resources.getString(R.string.str_humo)))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(resources.getString(R.string.str_xalqaro)))
+        binding.tabLayout.addTab(
+            binding.tabLayout.newTab().setText(resources.getString(R.string.str_hammasi))
+        )
+        binding.tabLayout.addTab(
+            binding.tabLayout.newTab().setText(resources.getString(R.string.str_sevimli))
+        )
+        binding.tabLayout.addTab(
+            binding.tabLayout.newTab().setText(resources.getString(R.string.str_uzcard))
+        )
+        binding.tabLayout.addTab(
+            binding.tabLayout.newTab().setText(resources.getString(R.string.str_humo))
+        )
+        binding.tabLayout.addTab(
+            binding.tabLayout.newTab().setText(resources.getString(R.string.str_xalqaro))
+        )
     }
 
 }
